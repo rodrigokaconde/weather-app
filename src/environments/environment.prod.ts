eb8fd6df9d3ec3e8fbea7cbe96dd4a6a68dc233f
rodrigokaconde <rodrigokaconde@yahoo.com.br>
2021-09-16 21:28:30 -0300

    create home and bookmarks, configure icon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '3cea0c535cd6bbe11132bdf2ae87ac89',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '3cea0c535cd6bbe11132bdf2ae87ac89',
 };
 
 /*
